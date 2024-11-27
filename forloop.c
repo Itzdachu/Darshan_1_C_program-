04419ffc2bb66a8b32e2b87c2a20a1019d18fd5d
Itzdachu <itzdachu30@gmail.com>
2024-11-27 09:09:31 +0530

    Create forloop.c

---
+++ forloop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i;  // Declare variable to control loop
+
+    // Using a for loop to print numbers from 1 to 10
+    for (i = 1; i <= 10; i++) {
+        printf("%d\n", i);  // Print current value of i
+    }
+
+    return 0;  // End of the program
+}
