d27bf6ace95de3edec6e92aae0f03a5567081448
Itzdachu <itzdachu30@gmail.com>
2024-11-27 09:11:14 +0530

    Create dowhile.c

---
+++ dowhile.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0, i = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while (i <= n) {
+        sum += i;
+        i++;
+    }
+    printf("Sum of numbers from 1 to %d is: %d\n", n, sum);
+    return 0;
+}
